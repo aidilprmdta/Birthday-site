06b4f488e9e040d01bb996535040b8f4bc5cbb19
Aidil <sekaiiprojectmv@gmail.com>
2025-06-14 11:42:02 +0700

    Update send-wish.js

--- pages/api/send-wish.js
+++ pages/api/send-wish.js
@@ -1,12 +1,19 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    console.log("Incoming request..."); // Tambah log!
+    console.log("BODY:", req.body); // Log body-nya
+
     const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
     const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+
+    console.log("TOKEN:", TELEGRAM_BOT_TOKEN);
+    console.log("CHAT_ID:", TELEGRAM_CHAT_ID);
 
     const { wish } = req.body;
 
     try {
       const telegramApiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+      console.log("Request to:", telegramApiUrl);
 
       const response = await fetch(telegramApiUrl, {
         method: "POST",
@@ -18,10 +25,13 @@ export default async function handler(req, res) {
       });
 
       const data = await response.json();
+      console.log("Telegram Response:", data);
+
       if (!response.ok) throw new Error(JSON.stringify(data));
 
       res.status(200).json({ message: "Terkirim ke Telegram!" });
     } catch (error) {
+      console.error("Error:", error);
       res.status(500).json({ error: error.message });
     }
   } else {
